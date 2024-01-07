ff1a6a3375e205e8e3adced8646ecb6e43546d8f
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-07 19:20:02 +0000

    added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,45 @@
+// const request = require("request");
+// const [, , breed] = process.argv;
+
+// const breedUrl = breed.substring(0, 4);
+// const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedUrl
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.error('Failed to fetch data:', error);
+//     return;
+//   }
+
+//   const data = JSON.parse(body);
+//   if (data[0] == undefined) {
+//     console.log("Breed not found")
+//     return;
+//   }
+//   console.log(data[0].description);
+// });
+
 const request = require("request");
-const [, , breed] = process.argv;
 
-const breedUrl = breed.substring(0, 4);
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedUrl
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Failed to fetch data:', error);
-    return;
-  }
+  request(url, (error, response, body) => {
+    if (error) {
+      callback('Failed to fetch data: ' + error, null);
+      return;
+    }
 
-  const data = JSON.parse(body);
-  if (data[0] == undefined) {
-    console.log("Breed not found")
-    return;
-  }
-  console.log(data[0].description);
-});
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback('Breed not found', null);
+      return;
+    }
+
+    const description = data[0].description;
+    callback(null, description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
 
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,28 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Chartreux', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Chartreux is generally silent but communicative. Short play sessions, mixed with naps and meals are their perfect day. Whilst appreciating any attention you give them, they are not demanding, content instead to follow you around devotedly, sleep on your bed and snuggle with you if you’re not feeling well.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+});
